2db41071158bc0fafa3bbcb93bd274c93acbcf1f
Axieof <pritheev24@gmail.com>
2022-01-19 16:04:52 +0800

    first commit

---
+++ src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	credential := options.Credential{
+		Username: "admin",
+		Password: "admin",
+	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://0.0.0.0:8059").SetAuth(credential))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//clientnew, err := mongo.Connect(context.TODO(), client)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
+}
